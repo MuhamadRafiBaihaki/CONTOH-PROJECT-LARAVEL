199fe06f9f46af9a827356e5eb3edd6ecdb0f056
MuhamadRafiBaihaki <rafibaihaki33@gmail.com>
2023-05-23 15:02:23 +0700

    perubahan praktikum 11

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -9,7 +9,7 @@
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-info">
   <div class="container">
-    <a class="navbar-brand" href="/toko">B-STORE</a>
+    <a class="navbar-brand" href="/toko">BEKI-STORE</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
